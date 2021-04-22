8c6d8c7162318d13a73784ee2c19e3043b41756d
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-22 21:41:36 +0300

    The call and buy callbacks

--- main.py
+++ main.py
@@ -35,3 +35,21 @@ instrument_id=data[choose_price[buy]["id"]]
 profit=data[choose_price][buy]["profit"]
 print("Choose you want to buy")
 print("price:",choose_price,"side:call","instrument_id:",instrument_id,"profit:",profit)
+
+#put instrument id to buy
+buy_check,id=api.buy_digital(amount,instrument_id)
+if buy_check:
+    print("wait for check win")
+    #Check win
+    while True:
+        check_close,win_money=api.check_win_digital(id)
+        if check_close:
+            if float(win_money)>0:
+                win_money=("%.2f" %(win_money))
+                print("You win",win_money,"money")
+            else:
+                print("you loose")
+            break
+    api.unsubscribe_strike_list(buy)
+else:
+    print("Fail to buy, Please run again, digit i
