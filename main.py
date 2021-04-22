e3b33243b4cd93408c7c4af51ec923b9776cbb54
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-22 21:39:45 +0300

    Working on subscribing on a list of stocks

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ candle = api.get_realtime_candles(goal, size)
 key = list(candle)[0]
 current_price = candle[key]['close']
 print("Current price", current_price)
+
+duration=1
+amount=1
+buy="call"
+#api.subscribe_strike_list(goal)
+data=api.get_realtime_strike_list(goal,duration)
+price_list=list(data.keys())
+close_current_price=[]
+for p_list in price_list:
+    close_current_price.append(abs(current_price-float(p_list)))
+most_close_price=price_list[close_current_price.index(min(close_current_price))]
+print("Most close price: {}\nCurrent price: {}".format(most_close_price,current_price))
+print(data[str(most_close_price)])
