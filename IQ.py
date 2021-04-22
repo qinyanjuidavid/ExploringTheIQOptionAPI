29adec88bf059615bed4c6386a0a73dadf99a75b
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-22 21:44:10 +0300

    Working on the Current Prices of the Specific Stock

--- IQ.py
+++ IQ.py
@@ -6,3 +6,19 @@ api.connect()
 api.start_candles_stream("EURUSD",1,2)
 candle=api.get_realtime_candles("EURUSD",1)
 print(candle)
+
+key=list(candle)[0]
+current_price=candle[key]['close']
+api.change_balance("PRACTICE")
+balance=api.get_balance()
+print("Balance:",balance)
+profit=api.get_all_profit()
+#print("Profit:",profit)
+real=api.get_all_realtime_candles()
+#print("Get all realtime:",real)
+#print(api.get_candles("EURUSD",1,111,time.time()))
+#Checking for all open markets
+#print(api.get_all_open_time())
+checkIfOpen=api.get_all_open_time()#Check if the market is open,forex,cfd,crypto,digital,binary,turbo
+print(checkIfOpen["digital"]["EURUSD"]["open"])
+print(api.get_top_assets_updated("forex"))
