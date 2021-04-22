642770fe151483b90c0f09d0ed22e87dfc089cdf
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-04-22 21:44:40 +0300

    Binary trading

--- IQ.py
+++ IQ.py
@@ -22,3 +22,17 @@ real=api.get_all_realtime_candles()
 checkIfOpen=api.get_all_open_time()#Check if the market is open,forex,cfd,crypto,digital,binary,turbo
 print(checkIfOpen["digital"]["EURUSD"]["open"])
 print(api.get_top_assets_updated("forex"))
+
+while True:
+    api.start_candles_stream("EURUSD",1,2)
+    candle=api.get_realtime_candles("EURUSD",1)
+    key=list(candle)[0]
+    current_price=candle[key]['close']
+    print("Current Price: {}".format(current_price))
+    print(type(current_price))
+    if current_price <= 1.4:
+        time.sleep(1)
+        #c=api.buy(1,"EURUSD","call",1)
+        check,order_id=api.buy_order("forex","USDCAD","buy",100,1,"market")
+        print("Buying")
+        print(api.get_async_order(order_id))
